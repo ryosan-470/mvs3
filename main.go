d7856a33c62e77cb8208d8ce84dcea6f13f7dc22
Ryosuke Sato <rskjtwp@gmail.com>
2018-04-03 13:11:39 +0900

    Add upload to S3

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -23,6 +24,10 @@ var (
 	OriginRegion string
 	// TargetFileName password if it is encrypted
 	Password string
+	// TargetBucket is a bucket name to upload document which downloaded from OriginBucket
+	TargetBucket string
+	// TargetRegion is AWS S3 region like "ap-northeast-1"
+	TargetRegion string
 )
 
 func main() {
@@ -80,8 +85,42 @@ func unzipWithPassword() ([]string, error) {
 	return compressedFileList, nil
 }
 
-func extractTarGz(targzList []string) {
+func extractTarGz(targzList []string, savedDir string) {
 	for _, targz := range targzList {
-		archiver.TarGz.Open(targz, "tmp")
+		archiver.TarGz.Open(targz, savedDir)
+		log.Printf("Extract %s to %s\n", targz, savedDir)
 	}
+}
+
+func uploadToS3(dir string) error {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(TargetRegion),
+	}))
+	uploader := s3manager.NewUploader(sess)
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", dir, err)
+			return err
+		}
+		filename := path[len(dir):]
+		f, err := os.Open(path)
+		stat, err := f.Stat()
+		if !stat.IsDir() {
+			result, err := uploader.Upload(&s3manager.UploadInput{
+				Bucket: aws.String(TargetBucket),
+				Key:    aws.String(filename),
+				Body:   f,
+			})
+			if err != nil {
+				return fmt.Errorf("Failed to upload file %v", err)
+			}
+			log.Printf("Upload: %s\n", result.Location)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("error walking the path %q: %v\n", dir, err)
+	}
+	return nil
 }
